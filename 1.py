a05227df952a771195082cc46cc8e44cbccf6f57
Zhang Yue Xin <azyxa@163.com>
2018-03-14 19:36:27 +0800

    完善了书籍信息

--- 1.py
+++ 1.py
@@ -13,9 +13,9 @@ web = ''.join(url)
 html = urlopen(web)
 # html = urlopen('https://api.douban.com/v2/book/1220562')
 
-status = {'title':1, 'author':1, "pubdate":1, "publisher":1, 'summary':1, 'price':1, 'pages':1, "isbn10":1,"isbn13":1}
- 
-hjson = json.loads(html.read())
+status = {'title':1, 'author':1, 'summary':1, 'rating':1, 'tags':1, "pubdate":1, "publisher":1, 'price':1, 'pages':1, "isbn10":1, "isbn13":1}
+tags = {} 
+hjson = json.loads(html.read().decode('utf-8'))
 # dict={}
 # a = hjson['rating'] ['average']#这里是得到花括号里面的子项
 # print (a)
@@ -26,8 +26,22 @@ hjson = json.loads(html.read())
 # 先考虑title和author 把他们存在dict当中
 for info in status:
     status[info]= hjson[info]
-    print(info,':\n',status[info])
+    if info == 'summary':
+        print(info,':\n',status[info])
+    elif info == 'author':
+    	print(info,':',status[info][0])
+    elif info == 'tags':
+    	print("tags:")
+    	for i in status[info]:
+    		print (i['name'],end=" ")
+    	print(' ')
+    elif info =='rating':
+    	print('rating:',hjson['rating'] ['average'])
+    else:
+        print(info,':',status[info])
+# print('rating:',hjson['rating'] ['average'])
 
 # dict = {'a':1,'v':2}
 # for i in dict :
 #     print(i)
+
