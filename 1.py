f971225775c1483dae8dfb5e5bb9924712bcda74
Zhang Yue Xin <azyxa@163.com>
2018-03-15 15:55:53 +0800

    ver0.3

--- 1.py
+++ 1.py
@@ -1,47 +1,51 @@
 #-*-coding:utf-8-*-
-# import requests
-# response = str(requests.get('https://api.douban.com/v2/book/1220562'))
-
-
 
 from urllib.request import urlopen
 import json
+
 url = ['https://api.douban.com/v2/book/']
-number = input('输入图书编码：')
-url.append ( str(number))
-web = ''.join(url)
-html = urlopen(web)
-# html = urlopen('https://api.douban.com/v2/book/1220562')
+status = {'title':1, 'author':1, 'summary':1, 'rating':1, 'tags':1, "pubdate":1, "publisher":1, 'price':1, 'pages':1, "isbn10":1, "isbn13":1} 
 
-status = {'title':1, 'author':1, 'summary':1, 'rating':1, 'tags':1, "pubdate":1, "publisher":1, 'price':1, 'pages':1, "isbn10":1, "isbn13":1}
-tags = {} 
-hjson = json.loads(html.read().decode('utf-8'))
-# dict={}
-# a = hjson['rating'] ['average']#这里是得到花括号里面的子项
-# print (a)
+def GetNmuber():
+    inpu = input('输入图书编码：')
+    try:
+        number = None
+        number = int(inpu)
+    except ValueError as e:
+        print("请输入正确的只包含数字的图书编码。")
+        return 
+    url.append(inpu)
+    return GetUrl(url)
 
+def GetUrl(url):
+    web = ''.join(url)
+    try:
+        html = urlopen(web)
+    except:
+        print('未找到书目，请输入正确的图书编码')
+        return
+    return OutPutInfo(html)
+    
 
-# 首先要得到书名'title'、评分'rating'(并打印出平均分)、作者名'author'然后按顺序打印出来
+def OutPutInfo(html):
+    hjson = json.loads(html.read().decode('utf-8'))
+    for info in status:
+        status[info]= hjson[info]
+        if info == 'summary':
+            print(info,':\n',status[info])
+        elif info == 'author':
+            if len(status[info])!=0:
+                print(info,':',status[info][0])
+            else:
+                print(info,':','未找到')
+        elif info == 'tags':
+            print("tags:")
+            for i in status[info]:
+                print (i['name'],end=" ")
+            print(' ')
+        elif info =='rating':
+            print('rating:',hjson['rating'] ['average'])
+        else:
+            print(info,':',status[info])
 
-# 先考虑title和author 把他们存在dict当中
-for info in status:
-    status[info]= hjson[info]
-    if info == 'summary':
-        print(info,':\n',status[info])
-    elif info == 'author':
-    	print(info,':',status[info][0])
-    elif info == 'tags':
-    	print("tags:")
-    	for i in status[info]:
-    		print (i['name'],end=" ")
-    	print(' ')
-    elif info =='rating':
-    	print('rating:',hjson['rating'] ['average'])
-    else:
-        print(info,':',status[info])
-# print('rating:',hjson['rating'] ['average'])
-
-# dict = {'a':1,'v':2}
-# for i in dict :
-#     print(i)
-
+GetNmuber()
