3c981ebc46fcb051c33887c9d70130da1012debf
Zhang Yue Xin <azyxa@163.com>
2018-03-15 16:57:06 +0800

    完善了信息

--- 1.py
+++ 1.py
@@ -4,7 +4,8 @@ from urllib.request import urlopen
 import json
 
 url = ['https://api.douban.com/v2/book/']
-status = {'title':1, 'author':1, 'summary':1, 'rating':1, 'tags':1, "pubdate":1, "publisher":1, 'price':1, 'pages':1, "isbn10":1, "isbn13":1} 
+status = {'title':1, 'origin_title':1,'alt_title':1, 'subtitle': '','tags':1,  'author':1, 'author_intro':1, 'summary':1, 'rating':1, 'ebook_url':1,
+          'translator':1, "pubdate":1, "publisher":1, 'price':1, 'binding':1, 'ebook_price':1,'catalog':1,'pages':1, "isbn10":1, "isbn13":1} 
 
 def GetNmuber():
     inpu = input('输入图书编码：')
@@ -30,22 +31,25 @@ def GetUrl(url):
 def OutPutInfo(html):
     hjson = json.loads(html.read().decode('utf-8'))
     for info in status:
-        status[info]= hjson[info]
-        if info == 'summary':
-            print(info,':\n',status[info])
-        elif info == 'author':
-            if len(status[info])!=0:
+        try:
+            status[info]= hjson[info]
+            if len(status[info])==0:
+                pass
+            elif info == 'summary' or info =='author_intro':
+                print(info,':\n',status[info])
+            elif info == 'author' or info =='translator':
                 print(info,':',status[info][0])
+            elif info == 'tags':
+                print("tags:")
+                for i in status[info]:
+                    print (i['name'],end=" ")
+                print(' ')
+            elif info =='rating':
+                print('rating:',hjson['rating'] ['average'])
             else:
-                print(info,':','未找到')
-        elif info == 'tags':
-            print("tags:")
-            for i in status[info]:
-                print (i['name'],end=" ")
-            print(' ')
-        elif info =='rating':
-            print('rating:',hjson['rating'] ['average'])
-        else:
-            print(info,':',status[info])
+                print(info,':',status[info])
+        except:
+            pass
+
 
 GetNmuber()
